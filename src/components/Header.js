8419b7fcd0cc00f12a5d58d2a5f5067548dd2ba1
Henry <53154228+sickerman@users.noreply.github.com>
2021-10-25 00:52:14 -0400

    Header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header({ handleInput, search }) {
   return (
     <header className='header'>
       <h1>The Shoppies</h1>
-      <h6>Submission by Henry Oke.</h6>
+      <h6>by Henry Oke.</h6>
       <Search handleInput={handleInput} search={search} />
     </header>
   )
